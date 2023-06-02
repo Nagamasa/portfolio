8d47aa4ba457ed047d9331afdb42f5f28fd4ed5d
Nagamasa <nagamasa1511@gmail.com>
2023-06-03 01:28:00 +0900

    ビルド設定変更

--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,9 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    // `root` からの相対パスで指定する
+    outDir: './docs',
   }
 })
