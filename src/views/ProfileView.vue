e3b13c72c8ca23248d8645d452d71dce96b4dc1b
Nagamasa <nagamasa1511@gmail.com>
2023-06-08 21:03:10 +0900

    profile 日付修正

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -10,7 +10,7 @@ import LeafRow from '../components/LeafRow.vue'
     <LeafRow title="2017.7 ~ 2019.9" sentence=" 株式会社アルカディアプラス入社"></LeafRow>
     <LeafRow title="" sentence="大手住宅設備メーカーにてシステム開発に従事" substance="見積もり発注システムの開発・保守" language="[使用言語] Java, JavaScript" db="[DB] SQL Server"  versioncontrol="[バージョン管理] SVN"></LeafRow>
     <br>
-    <LeafRow title="2019.10 ~ 2020.5" sentence="大手SIerにてシステム開発に従事" substance="ログ分析ツールの改修・対話型サービスにおけるアプリケーション開発" language="[使用言語] Node.js" framework="[フレームワーク] Express" versioncontrol="[バージョン管理] Git" other="[その他] Docker, ElasticSearch, Kibana, ansible"></LeafRow>
+    <LeafRow title="2019.10 ~ 2020.6" sentence="大手SIerにてシステム開発に従事" substance="ログ分析ツールの改修・対話型サービスにおけるアプリケーション開発" language="[使用言語] Node.js" framework="[フレームワーク] Express" versioncontrol="[バージョン管理] Git" other="[その他] Docker, ElasticSearch, Kibana, ansible"></LeafRow>
     <br>
     <LeafRow title="2020.9 ~ " sentence="株式会社エラン入社" ></LeafRow>
     <br>
